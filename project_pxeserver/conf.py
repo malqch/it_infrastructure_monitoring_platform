1181172556f5b97945bdd669cdb48663209d37b0
malqch <35054344+malqch@users.noreply.github.com>
2021-04-20 15:12:42 +0800

    Update conf.py

--- project_pxeserver/conf.py
+++ project_pxeserver/conf.py
@@ -1,4 +1,4 @@
 # AUTH = 'http://localhost:8000/sys/api/auth/'
 # SCRIPT_PATH = '/home/rock/test_script'
 # ASSET_URL = 'http://localhost:8001/asset/api/'
-AUTOMATION_URL = 'http://10.10.10.130:8004/automation/api/'
+AUTOMATION_URL = 'http://localhost:8004/automation/api/'
